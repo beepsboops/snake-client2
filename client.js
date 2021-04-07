13ffa554f5d6bbaf2557969d3b17ec5d614a7f54
A <awongpro7@gmail.com>
2021-04-07 21:34:10 +0000

    Imported connect function to play.js; created client.js file

---
+++ client.js
@@ -0,0 +1,25 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({
+    host: '135.23.222.131',
+    port: 50542
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  conn.on('data', data => console.log(data));
+
+  conn.on('connect', connectionEstablished => console.log("Successfully connected to game server"));
+
+  conn.on('connect', () => {
+    conn.write("Name: 604");
+  });
+
+  return conn;
+};
+
+module.exports = connect;
