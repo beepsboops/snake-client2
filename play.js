77d8c85588ef7db95f5273a98dc9cc60298a29da
A <awongpro7@gmail.com>
2021-04-08 00:56:50 +0000

    Moved input setupInput function to separate file to be exported

--- play.js
+++ play.js
@@ -1,29 +1,10 @@
-// const net = require('net');
-
 const connect = require('./client');
+const setupInput = require('./input');
+
 console.log('Connecting ...');
 connect(); // const conn = connect
 
 // connection func returns connection obj -> write to server
 // 
 
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
- const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  const handleUserInput = function() {
-    stdin.on('data', (key) => {
-      if (key === '\u0003') {
-        process.exit();
-      }
-    })
-  }
-  handleUserInput()
-  return stdin;
-};
 setupInput();
